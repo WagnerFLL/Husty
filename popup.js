6efc752e4b5cec7e7b65867e0e56bae4ed634f0b
WagnerFLL <wsf@ic.ufal.br>
2018-12-19 22:45:38 -0300

    Inject script and get page title :syringe:

--- popup.js
+++ popup.js
@@ -1,20 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var checkPageButton = document.getElementById('checkPage');
-  checkPageButton.addEventListener('click', function() {
+// Inject the payload.js script into the current tab after the popout has loaded
+window.addEventListener('load', function (evt) {
+  chrome.extension.getBackgroundPage().chrome.tabs.executeScript(null, {
+    file: 'payload.js'
+  });;
+});
 
-    chrome.tabs.getSelected(null, function(tab) {
-      d = document;
+// Listen to messages from the payload.js script and write to popout.html
+chrome.runtime.onMessage.addListener(function (message) {
+  document.getElementById('pagetitle').innerHTML = message;
+  document.getElementById('checkPage').innerHTML = "O que eu quiser";
+});
 
-      var f = d.createElement('form');
-      f.action = 'http://gtmetrix.com/analyze.html?bm';
-      f.method = 'post';
-      var i = d.createElement('input');
-      i.type = 'hidden';
-      i.name = 'url';
-      i.value = tab.url;
-      f.appendChild(i);
-      d.body.appendChild(f);
-      f.submit();
-    });
-  }, false);
-}, false);
+// document.addEventListener('DOMContentLoaded', function() {
+//   var checkPageButton = document.getElementById('checkPage');
+//   checkPageButton.addEventListener('click', function() {
+//     alert(location.hostname);
+//   }, false);
+// }, false);
