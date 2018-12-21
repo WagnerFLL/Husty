6789c726bc765f4c378a39546ec1049405b4f33e
WagnerFLL <wsf@ic.ufal.br>
2018-12-20 22:22:24 -0300

    Get link by id :video_camera:

--- popup.js
+++ popup.js
@@ -8,12 +8,6 @@ window.addEventListener('load', function (evt) {
 // Listen to messages from the payload.js script and write to popout.html
 chrome.runtime.onMessage.addListener(function (message) {
   document.getElementById('pagetitle').innerHTML = message;
+  console.log(message);
   document.getElementById('checkPage').innerHTML = "O que eu quiser";
 });
-
-// document.addEventListener('DOMContentLoaded', function() {
-//   var checkPageButton = document.getElementById('checkPage');
-//   checkPageButton.addEventListener('click', function() {
-//     alert(location.hostname);
-//   }, false);
-// }, false);
