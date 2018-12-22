2e87d13ad7313a5c41dfd7b0921e824eed413b39
WagnerFLL <wsf@ic.ufal.br>
2018-12-22 13:15:20 -0300

    Add downloader

--- popup.js
+++ popup.js
@@ -8,6 +8,31 @@ window.addEventListener('load', function (evt) {
 // Listen to messages from the payload.js script and write to popout.html
 chrome.runtime.onMessage.addListener(function (message) {
   document.getElementById('pagetitle').innerHTML = message;
-  console.log(message);
-  document.getElementById('checkPage').innerHTML = "O que eu quiser";
+  downloadURI(message, message);
+  saveChanges(message);
+  document.getElementById('checkPage').innerHTML = "Bt_1";
 });
+
+function saveChanges(textarea) {
+  // Get a value saved in a form.
+  var theValue = textarea;
+  // Check that there's some code there.
+  if (!theValue) {
+    message('Error: No value specified');
+    return;
+  }
+  // Save it using the Chrome extension storage API.
+  chrome.storage.local.set({'value': theValue}, function() {
+    // Notify that we saved.
+    console.log("Saved");
+    // message('Settings saved'); RD\'  \X '
+  });
+}
+
+function downloadURI(uri, name) 
+{
+    var link = document.createElement("a");
+    link.download = name;
+    link.href = uri;
+    link.click();
+}
